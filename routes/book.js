7255fb67cb9d03a8e53057f1a01e1fe1a3adfe3f
MaximeBress <maxime.bressange@gmail.com>
2016-12-06 10:41:56 +0100

    Add routing files for book and type

---
+++ routes/book.js
@@ -0,0 +1,25 @@
+var express = require('express')
+var router = express.Router()
+
+var bodyParser = require('body-parser')
+
+router.get("/", (req, res) => {
+	// TODO GET Library
+	// should return a json that contains the list of all books
+})
+router.get("/:bookId", (req, res) => {
+	// TODO GET Book
+	// should return a json that contains the book
+})
+router.post("/:bookId", (req, res) => {
+  // TODO POST book
+  // should create the book in mongoDb
+})
+router.delete("/:bookId", (req, res) => {
+  // TODO DELETE book
+  // should delete the book in mongoDb
+})
+router.put("/:bookId", (req, res) => {
+  // TODO PUT book
+  // should update the book in mongoDb
+})
