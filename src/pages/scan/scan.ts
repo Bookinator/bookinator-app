4472d2ed789d00d800d04a5db4572f8be203c7a4
Benji <b.illumin8@gmail.com>
2016-12-08 12:02:41 +0100

    Scan test

--- src/pages/scan/scan.ts
+++ src/pages/scan/scan.ts
@@ -1,22 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner } from 'ionic-native';
 
-/*
-  Generated class for the Scan page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+
 @Component({
   selector: 'page-scan',
   templateUrl: 'scan.html'
 })
 export class ScanPage {
 
-  constructor(public navCtrl: NavController) {}
-
-  ionViewDidLoad() {
-    console.log('Hello ScanPage Page');
+  constructor(public navCtrl: NavController) {
+  	
   }
 
+  scan(){
+  	BarcodeScanner.scan().then((barcodeData) => {
+		console.log("YES WE CAN");
+	}, (err) => {
+	    console.log("NOT POSSIBLE");
+	});
+  }
 }
